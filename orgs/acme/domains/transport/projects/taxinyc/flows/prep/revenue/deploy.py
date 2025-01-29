26a835388bf571d1eaf933c99719701ef5d2b430
azzlack <azzlack>
2025-01-29 13:29:17 +0000

    .

--- orgs/acme/domains/transport/projects/taxinyc/flows/prep/revenue/deploy.py
+++ orgs/acme/domains/transport/projects/taxinyc/flows/prep/revenue/deploy.py
@@ -59,7 +59,8 @@ run_job_by_name(dbutils=dbutils, job_name=response['job_name'])
 
 # COMMAND ----------
 
-# os.environ['PIPELINE_ENV'] = 'prod'
+import os
+os.environ['PIPELINE_ENV'] = 'prod'
 # Deploy jobs based on deployment.yml, in dev mode
 prod_response = autojob(env="prod")
 
